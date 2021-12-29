9131d850098f7db5798802497d35699d55be8a60
NathanNCN <78819516+NathanNCN@users.noreply.github.com>
2021-12-28 19:00:53 -0500

    finished game

--- main.js
+++ main.js
@@ -13,40 +13,46 @@ function updatePlayer(){
 }
 
 function restGame(){
-    computerScore = 0;
-    playerScore = 0;
-    scoreBoardPlayer.innerHTML = playerScore;
-    scoreBoardComputer.innerHTML = computerScore;
+    if (computerScore===5 || playerScore===5){
+        computerScore = 0;
+        playerScore = 0;
+        scoreBoardPlayer.innerHTML = playerScore;
+        scoreBoardComputer.innerHTML = computerScore;
+    }
+
 
 }
 function isWinner(){
     if (computerScore === 5){
         winner.innerHTML = 'I won';
-        restGame();
     } else if (playerScore === 5){
         winner.innerHTML = 'You Win';
-        restGame();
     }
 }
 
+
 function round(playerSelection, computerSelection){
+    restGame();
     if (playerSelection === computerSelection) {
         winner.innerHTML = 'We Draw, we both picked ' + playerSelection;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
         winner.innerHTML = 'You won. You choose rock I choose scissors';
         updatePlayer();
+        isWinner()
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         winner.innerHTML = 'You won. You choose paper I choose rock';
         updatePlayer();
+        isWinner()
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         winner.innerHTML = 'You won. You choose scissors I choose paper';
         updatePlayer();
+        isWinner()
     } else {
         winner.innerHTML = 'I won. you choose ' + playerSelection + ' I chose ' + computerSelection
         computerScore += 1;
         scoreBoardComputer.innerHTML = computerScore;
+        isWinner()
     }
-    isWinner();
 }
 
 let buttonR = document.getElementById('rockButton');
